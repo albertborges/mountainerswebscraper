a1bbb12ad5f69d8f02095a348baf18bdfac3f562
Albert Borges <aeb3bs@gmail.com>
2018-12-22 02:40:45 -0500

    Adding appropriate imports.

--- scraper.py
+++ scraper.py
@@ -5,6 +5,8 @@ import os
 from selenium import webdriver  
 from selenium.webdriver.common.keys import Keys  
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 import smtplib
 
